12e6de4f9905ad1244c26c6b33648f759ea96f92
Kdusrb7743 <104414697+Kdusrb7743@users.noreply.github.com>
2022-05-26 21:59:32 +0900

    Add files via upload

--- project_main.py
+++ project_main.py
@@ -4,13 +4,19 @@ from tarfile import PAX_FIELDS
 from tkinter import *
 from tkinter import font
 from tkinter import ttk
-
 from numpy import pad
 
+from http.client import HTTPSConnection
 
 window = Tk()
-window.geometry("400x600-350+100") # 앞에는 크기 , 뒤에는 좌표
+window.geometry("500x700-350+100") # 앞에는 크기 , 뒤에는 좌표
 
+CityStr = StringVar()
+
+Url = None
+conn = None
+SIGUN_NM_str = None
+        
 def InitScreen():
     fontTitle = font.Font(window, size=18, weight='bold',family='돋움체')
     fontNormal = font.Font(window, size=15, weight='bold')
@@ -26,33 +32,87 @@ def InitScreen():
     framemap = Frame(window)
     framemap.pack(side="top", fill="x")
     
-    # 타이틀
+    # 1. 타이틀
     MainText = Label(frameTitle, font = fontTitle, text='편의시설 - 카페 편의점 약국')
     MainText.pack(anchor="center",fill="both")
 
-    data = ["임시 저장", "1", "2"]
-    cityCombo = ttk.Combobox(frameCombo, values=data, font=fontNormal)
-    cityCombo.set("시군구")
+    # 2. 시군구, 이메일 
+    data = ["수원시", "성남시", "용인시", "안양시", "안산시", "과천시", "광명시", "광주시", "군포시", "부천시", "시흥시", "김포시", "안성시"\
+        , "오산시", "의왕시", "이천시", "평택시", "하남시", "화성시", "여주시", "고양시", "구리시", "남양주시", "동두천시", "양주시", "의정부시"
+        , "파주시", "포천시", "양평군", "연천군", "포천군"]
+    cityCombo = ttk.Combobox(frameCombo, values=data, font=fontNormal, textvariable=CityStr)
+    cityCombo.set("경기도 시군구")
+    #cityCombo.bind()   ---------------------------------------------콤보박스 선택시 이벤트 해주는 바인드 부분 할 차례
     cityCombo.pack(side='left', padx=30)
-
-    emailbutton = Button(frameCombo, text="이메일", padx=10, pady=10)
-    emailbutton.pack(side='right', padx=20)
     
-    Cafebutton = Button(frameselect, text="카페", padx=10, pady=10)
-    Cafebutton.grid(row=0, column=0, padx=60)
-    Conveniencebutton = Button(frameselect, text="편의점", padx=10, pady=10)
+    global SIGUN_NM_str
+    SIGUN_NM_str = '&SIGUN_NM='+CityStr.get()      # 시군구 요청인자 &SIGUN_NM=땡땡시
+
+    emailbutton = Button(frameCombo, text="이메일", padx=10, pady=10, command=Print)
+    emailbutton.pack(side='right', padx=50)
+    
+    # 3. 카페, 편의점, 약국 버튼
+    #CaftUrl = "https://openapi.gg.go.kr/Resrestrtcvnstr?KEY=9dff4350fafe400db05270b8161c46d3"      #카페
+    #ConvenienceUrl = "https://openapi.gg.go.kr/Genrestrtcate?KEY=9dff4350fafe400db05270b8161c46d3" #편의점
+    #PharmacyUrl = "https://openapi.gg.go.kr/Parmacy?KEY=9dff4350fafe400db05270b8161c46d3"          #약국
+    
+    # 사업자명 BIZPLC_NM
+
+    #conn = HTTPSConnection(Url)
+    global conn
+    #conn = HTTPSConnection(Url)
+
+    # 1. 위에서 경기도 시군구에서 시군구 요청인자 + 카페,편의점,약국 버튼 눌러서 만든 완성된 Url 가지고 인터넷 불러가서 xml 연다.
+    # 2. 
+
+    Cafebutton = Button(frameselect, text="카페", padx=20, pady=20, command=CaftUrl)             # 카페 버튼 누르면 밑에 리스트 박스에 정보 송출
+    Cafebutton.grid(row=0, column=0, padx=65)
+    Conveniencebutton = Button(frameselect, text="편의점", padx=20, pady=20, command=ConvenienceUrl)
     Conveniencebutton.grid(row=0, column=1)
-    Pharmacybutton = Button(frameselect, text="약국", padx=10, pady=10)
-    Pharmacybutton.grid(row=0, column=2, padx=60)
+    Pharmacybutton = Button(frameselect, text="약국", padx=20, pady=20, command=PharmacyUrl)
+    Pharmacybutton.grid(row=0, column=2, padx=65)
 
 
-    # 시(군) 선택 및 검색창
-    #global Searchcity
-    #controlbar = Searchcity(frameCombo)
+    # 4. 리스트 및 상세정보
+    LBscrollbar = Scrollbar(framelist)
+    left_listbox = Listbox(framelist, font=fontNormal, width = 20,\
+        yscrollcommand=LBscrollbar.set)
+    left_listbox.pack(side='left')
+    LBscrollbar.pack(side='left')
+    right_listbox = Listbox(framelist,font=fontNormal, width = 20, \
+        yscrollcommand=LBscrollbar.set)
+    right_listbox.pack(side='right')
+
+    # 5. 지도 및 그래프 부분
+    # 추가
+
+    # 이후 이메일 처리, 리스트 스크롤 처리 xml api들여오기, 지도, 그래프
     
+    # URL 편의점 https://openapi.gg.go.kr/Resrestrtcvnstr?KEY=9dff4350fafe400db05270b8161c46d3
+    # URL 카페   https://openapi.gg.go.kr/Genrestrtcate?KEY=9dff4350fafe400db05270b8161c46d3
+    # URL 약국   https://openapi.gg.go.kr/Parmacy?KEY=9dff4350fafe400db05270b8161c46d3
 
 # 주제 : 편의시설: 카페, 편의점, 약국
+def Print():
+    global Url
+    global SIGUN_NM_str
+    print(Url)
+    print(SIGUN_NM_str)
+
+def CaftUrl():
+    global Url
+    Url = "https://openapi.gg.go.kr/Resrestrtcvnstr?KEY=9dff4350fafe400db05270b8161c46d3"
+
+def ConvenienceUrl():
+    global Url
+    Url = "https://openapi.gg.go.kr/Genrestrtcate?KEY=9dff4350fafe400db05270b8161c46d3"
+
+def PharmacyUrl():
+    global Url
+    Url = "https://openapi.gg.go.kr/Parmacy?KEY=9dff4350fafe400db05270b8161c46d3"
+
 
 InitScreen() # 화면 전체 구성
+
 
 window.mainloop()
